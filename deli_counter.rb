baab51a4371339f11746250dba611e3653d6f4ad
osama miro <o.miro650@gmail.com>
2017-12-18 20:17:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,18 +18,17 @@ def take_a_number(arr,str)
     arr.push(str)
     puts "Welcome, #{str}. You are number 1 in line."
   else
-    in_line = arr.size # Step 1
-  people = 0 # Step 2
-   
-  loop do 
-      if in_line < arr.size 
+    in_line = arr.size
+  people = 0 
+
+loop do
+      if in_line < arr.size
 
           puts "Welcome #{str}"
           people += 1
       else
           break
       end
-  end
-
+end
 
 end
