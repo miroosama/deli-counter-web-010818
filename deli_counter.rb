192dd8f4fd68b0e84d454fbad3fd5e4a7e886b08
osama miro <o.miro650@gmail.com>
2017-12-18 18:35:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def line(arr)
+
+  if arr == []
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    arr.each do |i|
+    customer = " #{arr.index(i)+1}. #{i}"
+    string += customer
+    end
+puts string
+  end
+end
+
+def take_a_number(arr,str)
+  if arr == []
+    puts "Welcome, #{str}. You are number 1 in line."
+  else
+  arr.push(str)
+end
+end
